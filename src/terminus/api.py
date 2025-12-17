aec25dae6e0a77e41988d52ccb2a5c3a1c311225
William Millet <z5692883@ad.unsw.edu.au>
2025-12-17 18:47:27 +1100

    Refactor api callable to remove near duplicate methods

--- src/terminus/api.py
+++ src/terminus/api.py
@@ -1,21 +1,25 @@
 from wsgiref.types import WSGIEnvironment, StartResponse
-from typing import Iterable
+from typing import Any, Iterable, Callable
 
 from terminus.types import HTTPMethod, Request, HTTPError
 from terminus.router import Router, RouteFn
 from terminus.response import Response
 from terminus.request_factory import RequestFactory
+from terminus.execution_pipeline import ExecutionPipeline
+
+type RouteDecorator = Callable[[RouteFn], RouteFn]
 
 class API:
     def __init__(self) -> None:
         self.router = Router()
+        self.pipeline = ExecutionPipeline()
     
     def __call__(self, environ: WSGIEnvironment,
                  start_response: StartResponse) -> Iterable[bytes]:
         """Entrypoint to the gunicorn web server"""
         
         method_str = environ["REQUEST_METHOD"]
-        if method_str not in HTTPMethod.__members__:
+        if method_str not in HTTPMethod:
             return Response.send_err(start_response, f"HTTP method '{method_str}' not recognised")
         method = HTTPMethod(method_str)
         
@@ -35,36 +39,21 @@ class API:
         
         return http_res.send()
     
-    def _build_route_decorator(self, method: HTTPMethod, path: str):
+    def _build_route_decorator(self, method: HTTPMethod, path: str) -> RouteDecorator:
         """Build a decorator function for some specific HTTP method"""
         def decorator(fn: RouteFn) -> RouteFn:
             self.router.register_route(method, path, fn)
             return fn
         return decorator
-        
-    def get(self, path: str):
-        return self._build_route_decorator(HTTPMethod.GET, path)
-
-    def post(self, path: str):
-        return self._build_route_decorator(HTTPMethod.POST, path)
     
-    def put(self, path: str):
-        return self._build_route_decorator(HTTPMethod.PUT, path)
-    
-    def delete(self, path: str):
-        return self._build_route_decorator(HTTPMethod.DELETE, path)
-
-    def patch(self, path: str):
-        return self._build_route_decorator(HTTPMethod.PATCH, path)
-
-    def trace(self, path: str):
-        return self._build_route_decorator(HTTPMethod.TRACE, path)
-
-    def options(self, path: str):
-        return self._build_route_decorator(HTTPMethod.OPTIONS, path)
-
-    def connect(self, path: str):
-        return self._build_route_decorator(HTTPMethod.CONNECT, path)
+    def __getattr__(self, name: str) -> Any:
+        uppercased = name.upper()
+        if uppercased in HTTPMethod:
+            def decorator_builder(path: str, **kwargs) -> RouteDecorator:
+                return self._build_route_decorator(HTTPMethod(uppercased), path, **kwargs) 
+            return decorator_builder
+        else:
+            raise AttributeError(f"API attribute '{name}' unknown")
 
 api = API()
 
